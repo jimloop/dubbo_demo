e42253e072cfdb50e8b2fa0acea69ff5abb398a5
jimloop <1447539855@qq.com>
2019-07-23 16:31:00 +0800

    provider

---
+++ src/main/java/Provider.java
@@ -0,0 +1,12 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+public class Provider {
+    public static void main(String[] args) throws IOException {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("provider.xml");
+        context.start();
+        System.out.println("dubbo服务端已启动...");
+        System.in.read();
+    }
+}
---
+++ src/main/java/com/dubbo/provider/service/impl/DemoServiceImpl.java
@@ -0,0 +1,10 @@
+package com.dubbo.provider.service.impl;
+
+import com.dubbo.api.service.DemoService;
+
+public class DemoServiceImpl implements DemoService {
+
+ public String sayHello(String name){
+     return "Hello "+ name;
+ }
+}
